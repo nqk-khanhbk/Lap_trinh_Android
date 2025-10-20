4fec92688d2a4f54ec72e190d9b68cd7543dd503
nqk-khanhbk <khanhbk0102@gmail.com>
2025-10-20 14:26:12 +0700

    Lập trình màn máy tính casio

---
+++ CalculatorApp/app/src/main/java/vn/edu/hust/calculatorapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package vn.edu.hust.calculatorapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_linear_layout)
+    }
+}
